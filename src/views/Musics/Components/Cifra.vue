39215a6a7f7929d46d7b959ffb8a98e2886435e8
Levi Manoel <levimanoel.deob@gmail.com>
2023-06-19 21:03:29 -0300

    [feat] show cipher when the music is selected

--- src/views/Musics/Components/Cifra.vue
+++ src/views/Musics/Components/Cifra.vue
@@ -2,6 +2,16 @@
   <main>
     {{ musica.no_musica }}
     {{ musica.ordem_cifra }}
+
+    <div>
+      <div v-for="parte of musica.ordem_cifra.split(';')">
+        <div v-for="linha of musica.partes_cifra[parte]" >
+          <el-text class="mx-1" style="white-space: pre;" type="primary">{{ linha.acordes }}</el-text>
+          <p class="mx-1">{{ linha.letra }}</p>
+        </div>
+        <br />
+      </div>
+    </div>
   </main>
 </template>
 
@@ -10,17 +20,32 @@ export default {
   props: ['musica'],
 
   data() {
-    return {}
+    return {
+      cifraFormatada: []
+    }
   },
 
   created() {
-    console.log('🚀\n🚀\n🚀\n || this.musica:', this.musica.ordem_cifra.split(';'))
+    this.formataCifra()
     
   },
 
   methods: {
     formataCifra() {
-      
+      const cifraFormatada = []
+      const partes = this.musica.ordem_cifra.split(';')
+
+      for (const parte of partes) {
+        for (const linha of this.musica.partes_cifra[parte]) {
+          cifraFormatada.push({
+            texto: linha.acordes, tipo: 'acordes'
+          }, {
+            texto: linha.letra, tipo: 'letra'
+          })
+        }
+      }
+
+      this.cifraFormatada = cifraFormatada
     }
   }
 }
