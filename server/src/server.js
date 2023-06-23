4dad4fe647f37b0b807728f4308d6fe4ea9607ef
Levi Manoel <levimanoel.deob@gmail.com>
2023-06-22 23:43:14 -0300

    [server] configure cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,11 @@
 import express from 'express'
 import routers from './routers/index.js'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
 
 app.use('/musicas', routers.musica)
 
